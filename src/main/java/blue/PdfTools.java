b2ed364aadf557c6bec5fe9428b03d8966b4f6af
Sean Willison <sean.willison@bcbsfl.com>
2018-10-22 14:19:04 -0400

    working on PDF decomposition

---
+++ src/main/java/blue/PdfTools.java
@@ -0,0 +1,16 @@
+
+package blue;
+
+// import org.apache.pdfbox.*;
+// import org.apache.pdfbox.pdmodel;
+
+public class PdfTools {
+        public static void main(String[] args) {
+                // PDFTextStripper stripper = new PDFTextStripper();
+                System.out.println("Cool");
+                // stripper.setStartPage(2);
+                // stripper.setEndPage(3);
+                // stripper.writeText( ... );
+
+        }
+}
